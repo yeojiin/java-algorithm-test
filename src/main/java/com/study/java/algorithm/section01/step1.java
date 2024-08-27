168ccec57226575f7f6b158329dc843f2a900cb1
jylim <jylim@12cm.co.kr>
2024-08-28 08:41:08 +0900

    Initial commit

---
+++ src/main/java/com/study/java/algorithm/section01/step1.java
@@ -0,0 +1,13 @@
+package com.study.java.algorithm.section01;
+
+import java.util.Scanner;
+
+public class step1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input1 = in.nextInt();
+        int input2 = in.nextInt();
+        System.out.println(input1 + input2);
+        return;
+    }
+}
---
+++ src/test/java/com/study/java/algorithm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.java.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
