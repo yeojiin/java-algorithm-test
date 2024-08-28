4324aaf73d231d4d6dd536b6bb8d2afe2f4dd531
jylim <jylim@12cm.co.kr>
2024-08-28 14:27:12 +0900

    feat: 1-7 회문 문자열

---
+++ src/main/java/com/study/java/algorithm/section01/step_07.java
@@ -0,0 +1,18 @@
+package com.study.java.algorithm.section01;
+
+import java.util.Scanner;
+
+public class step_07 {
+
+    // 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+    // 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+    // 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine().toLowerCase();
+        String reverse = new StringBuilder(in).reverse().toString();
+
+        String result = in.equals(reverse) ? "YES" : "NO";
+        System.out.println(result);
+    }
+}
